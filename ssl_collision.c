0d442022035d62fda5ab78d652e564c855e52109
bernardo m <mendesb@oregonstate.edu>
2025-10-15 14:56:55 -0700

    starting to create a hash function

--- ssl_collision.c
+++ ssl_collision.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-void trial_run(int trial_num);
+void weak_collision_trial(int trial_num);
 
 int main(int argc, char* argv[]) {
 
@@ -15,17 +15,34 @@ int main(int argc, char* argv[]) {
     printf("-------------------------------------------\n");
 
     for (int i = 1; i <= trials; i++) {
-        trial_run(i);
+        weak_collision_trial(i);
     }
 
 
     return 0;
 }
 
-void trial_run(int trial_num) {
-    printf("This is trial run %d\n", trial_num);
+unsigned char* hash_message(const char* message) {
+    EVP_MD_CTX = *mdctx;
+    unsigned int md_len;
+    unsigned char full_hash[EVP_MAX_MD_SIZE];
+
+    mdctx = EVP_MD_CTX_new();
+    EVP_DIGEST_Init_ex();
+}
+
+void weak_collision_trial(int trial_num) {
+    //printf("This is Weak Collision Trial run %d\n", trial_num);
 
     int count_to_break_hash = 0;
     // Write code to see how many attempts it will take a break the hash
     // of the first 24 bits of a hash
+
+    unsigned char target_hash[3];
+    unsigned char current_hash[3];
+
+    char target_msg[32];
+    char current_msg[32];
 }
+
+
